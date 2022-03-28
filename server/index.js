a480fba637788f60caf72727de99e72f8b215cfd
Heorhii <jorhesancho@gmail.com>
2022-03-28 23:53:40 +0300

    fixed server url on client

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 app.use(cors({
     origin: ['http://localhost:4000', 'http://localhost:3000', 'http://localhost:3001',
-        'https://aviasales-server.herokuapp.com/','https://sanchez-aviasales.herokuapp.com']
+        'https://sanchez-airplane-server.herokuapp.com/','https://sanchez-airplane-tickets.herokuapp.com']
 }))
 
 
