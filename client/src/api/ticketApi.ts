6c467f2a768bca196bd4ef3a9eb433e684267a12
Heorhii <jorhesancho@gmail.com>
2022-03-28 23:47:40 +0300

    fixed server url

--- client/src/api/ticketApi.ts
+++ client/src/api/ticketApi.ts
@@ -3,7 +3,7 @@ import {ITicket} from "../types/types";
 
 class TicketApi {
     async get(): Promise<ITicket[]> {
-        const urls = ['https://aviasales-server.herokuapp.com/', 'http://localhost:4000']
+        const urls = ['https://sanchez-airplane-server.herokuapp.com/', 'http://localhost:4000']
         const response = await axios.get<ITicket[]>(urls[1]);
         return response.data;
     }
